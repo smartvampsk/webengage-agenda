b2b7473856583ed68a3a4fa77d276b61cbe86f88
Santosh Khanal <smartvamps@gmail.com>
2021-08-15 13:04:26 +0545

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router';
 
+
+import App from './App.vue'
+import Index from './components/Index.vue'
+
+
+Vue.use(VueRouter);
 Vue.config.productionTip = false
+
+const routes = [
+  {
+    name: 'Index',
+    path: '/',
+    component: Index
+  },
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes });
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
